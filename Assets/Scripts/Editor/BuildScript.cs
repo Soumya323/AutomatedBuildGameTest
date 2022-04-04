bff2f0eea1625f1c2b06fee58e56503456fa6dfe
soumyaranjan@autovrse.in <>
2022-04-04 19:43:11 +0530

    update

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -32,43 +32,30 @@ public class BuildScript
         string buildVersion = "1", buildVersionCode = "1", buildNameForQuestBase = "AutomatedBuildQuest_QA";
         string timeStamp = "_" + System.DateTime.Now.Month.ToString() + "M" + System.DateTime.Now.Day.ToString() + "D" + System.DateTime.Now.Hour.ToString() + "H" + System.DateTime.Now.Minute.ToString() + "m";
 
-        /*
-                commandToValueDictionary = GetCommandLineArguments();
-        // Dictionary<string, string> commandToValueDictionary = GetCommandLineArguments();
-        commandToValueDictionary.TryGetValue(BuildVersionCommandQuest, out buildVersion);
-        commandToValueDictionary.TryGetValue(BuildVersionCodeCommandQuest, out buildVersionCode);
-        commandToValueDictionary.TryGetValue(BuildApkNameCommandQuest, out buildNameForQuest);
-        */
-
         List<string> scenePaths = new List<string>();
         foreach (EditorBuildSettingsScene scenePath in EditorBuildSettings.scenes)
         {
             scenePaths.Add(scenePath.path);
         }
         string[] allBuildScenes = scenePaths.ToArray();
-        //string[] defaultScene = { "Assets/Scenes/Scene1.unity", "Assets/Scenes/Scene2.unity", "Assets/Scenes/Scene3.unity" };
-        // BuildPipeline.BuildPlayer(defaultScene, "../BuildsOutput/Quest/AutomatedBuildQuest.apk", BuildTarget.Android, BuildOptions.None);
 
         buildVersion = PlayerSettings.bundleVersion;
         buildVersionCode = PlayerSettings.Android.bundleVersionCode.ToString();
-
-
         BuildPlayerOptions buildPlayerOptionsAndroid = new BuildPlayerOptions();
         buildPlayerOptionsAndroid.scenes = allBuildScenes;
-        //buildPlayerOptionsAndroid.locationPathName = "../BuildsOutput/Quest/" + buildNameForQuest + ".apk";
         string fullBuildNameForQuest = buildNameForQuestBase + "_" + buildVersion + "_" + buildVersionCode + timeStamp;
         buildPlayerOptionsAndroid.locationPathName = "BuildsOutput/Quest/Build/" + fullBuildNameForQuest + ".apk";
         buildPlayerOptionsAndroid.target = BuildTarget.Android;
         buildPlayerOptionsAndroid.options = BuildOptions.None;
 
-        // PlayerSettings.Android.useCustomKeystore = false;
         PlayerSettings.Android.useCustomKeystore = true;
         PlayerSettings.Android.keystoreName = "Assets/Others/KeyStore/userAutoMatedBuild.keystore";
         PlayerSettings.Android.keystorePass = "8dsp?Q7mS87fHxRG";
         PlayerSettings.Android.keyaliasName = "district m key";
         PlayerSettings.Android.keyaliasPass = "8dsp?Q7mS87fHxRG";
 
-        WriteBuildNameToFile(BuildTarget.StandaloneWindows64, fullBuildNameForQuest);
+        // Write the build name to file
+        WriteBuildNameToFile(BuildTarget.Android, fullBuildNameForQuest);
 
         BuildReport buildReport = BuildPipeline.BuildPlayer(buildPlayerOptionsAndroid);
         BuildSummary buildSummary = buildReport.summary;
@@ -85,15 +72,8 @@ public class BuildScript
             scenePaths.Add(scenePath.path);
         }
         string[] allBuildScenes = scenePaths.ToArray();
-        //string[] defaultScene = { "Assets/Scenes/Scene1.unity", "Assets/Scenes/Scene2.unity", "Assets/Scenes/Scene3.unity" };
-        /*
-        commandToValueDictionary = GetCommandLineArguments();
-        commandToValueDictionary.TryGetValue(BuildVersionCodeCommandPCVR, out buildVersion);
-        commandToValueDictionary.TryGetValue(BuildNameCommandPCVR, out buildNameForPCVR);
-        */
 
         buildVersion = PlayerSettings.bundleVersion;
-
         BuildPlayerOptions buildPlayerOptionsWindows = new BuildPlayerOptions();
         buildPlayerOptionsWindows.scenes = allBuildScenes;
         string fullBuildNameForPCVR = baseBuildNameForPCVR + "_" + buildVersion + "_" + timeStamp;
@@ -101,11 +81,11 @@ public class BuildScript
         buildPlayerOptionsWindows.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptionsWindows.options = BuildOptions.None;
 
+        // Write the build name to file
         WriteBuildNameToFile(BuildTarget.StandaloneWindows64, fullBuildNameForPCVR);
 
         BuildReport buildReport = BuildPipeline.BuildPlayer(buildPlayerOptionsWindows);
         BuildSummary buildSummary = buildReport.summary;
-        // BuildPipeline.BuildPlayer(defaultScene, "./Builds/Windows/WindowsTestBuild.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 
     /**************************************************************************************************************************************/
