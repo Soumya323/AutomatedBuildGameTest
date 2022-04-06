07c4081273104c283759da9f642d399f49f85be4
soumyaranjan@autovrse.in <>
2022-04-06 23:33:15 +0530

    update

--- Assets/Scripts/Code/AutoLightBakerPrepareScene.cs
+++ Assets/Scripts/Code/AutoLightBakerPrepareScene.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor;
+using UnityEditor.SceneManagement;
 #endif
 
 [ExecuteInEditMode]
